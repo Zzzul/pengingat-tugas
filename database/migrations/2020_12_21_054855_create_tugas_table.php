524472691e0af49e39a328376d8fc9a3d71e6889
Zzzul <807fahmi@gmail.com>
2020-12-21 12:52:48 +0700

    Create: new semester, matkul, and tugas migration

---
+++ database/migrations/2020_12_21_054855_create_tugas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTugasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('matkul_id')->constrained('matkuls');
+            $table->string('deskripsi');
+            $table->dateTime('batas_waktu');
+            $table->bigInteger('pertemuan_ke');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tugas');
+    }
+}
