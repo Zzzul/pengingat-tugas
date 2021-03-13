e3bb8614418862ed5ac7df7bb368a61d2aafa7c0
Zzzul <807fahmi@gmail.com>
2021-03-13 21:56:55 +0700

    Fix: error cause backtick on LivewireTraits

--- app/traits/LivewireAlert.php
+++ app/traits/LivewireAlert.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 trait LivewireAlert
 {
     public function showAlert($type, $message)
-    {`
+    {
         $this->alert($type, $message, [
             'position'          =>  'top',
             'timer'             =>  2000,
